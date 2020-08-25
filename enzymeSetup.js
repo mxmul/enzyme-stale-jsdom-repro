55ec7035b4466379b999114b79c0125916226b3a
Matt Mulder <muldmatt@gmail.com>
2020-08-24 23:41:39 -0700

    unmount enzyme components afterEach

--- enzymeSetup.js
+++ enzymeSetup.js
@@ -1,4 +1,25 @@
 const enzyme = require("enzyme");
 const Adapter = require("enzyme-adapter-react-16");
+const reactDOM = require("react-dom");
 
-enzyme.configure({ adapter: new Adapter() });
+const nodes = [];
+
+class ReactAdapterWithMountTracking extends Adapter {
+  createRenderer(options) {
+    let node;
+    if (options.mode === enzyme.EnzymeAdapter.MODES.MOUNT) {
+      node = options.attachTo || global.document.createElement("div");
+      nodes.push(node);
+    }
+
+    return super.createRenderer.call(this, { ...options, attachTo: node });
+  }
+}
+
+afterEach(() => {
+  nodes.forEach((node) => {
+    reactDOM.unmountComponentAtNode(node);
+  });
+});
+
+enzyme.configure({ adapter: new ReactAdapterWithMountTracking() });

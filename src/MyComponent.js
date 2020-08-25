83b703a2a0722df482398a72822ab8533a42e717
Matt Mulder <muldmatt@gmail.com>
2020-08-24 23:39:22 -0700

    abort fetch on unmount

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -7,11 +7,16 @@ module.exports = function LeakyComponent() {
   const [name, setName] = React.useState("Stranger");
 
   React.useEffect(() => {
-    fetch("https://swapi.dev/api/people/1/")
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://swapi.dev/api/people/1/", { signal })
       .then((resp) => resp.json())
       .then((data) => {
         setName(data.name);
       });
+
+    return () => controller.abort();
   }, []);
 
   return html`<p>Hello ${name}</p>`;
